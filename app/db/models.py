dc947321c3f05edc335c2532b9427beedf3a7e97
anushagh <hanagandianusha@gmail.com>
2025-09-13 13:18:15 +0530

    wip - setup db

---
+++ app/db/models.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Column, Integer, String, Float, Date, DateTime
+from .database import Base
+from datetime import datetime
+
+class Driver(Base):
+    __tablename__ = "drivers"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+class Earnings(Base):
+    __tablename__ = "earnings"
+    id = Column(Integer, primary_key=True, index=True)
+    driver_id = Column(Integer, index=True)
+    amount = Column(Float)
+    expenses = Column(Float)
+    date = Column(Date)
+
+class Trip(Base):
+    __tablename__ = "trips"
+    id = Column(Integer, primary_key=True, index=True)
+    driver_id = Column(Integer, index=True)
+    start_time = Column(DateTime, default=datetime.utcnow)
+    end_time = Column(DateTime, nullable=True)
+    status = Column(String)
